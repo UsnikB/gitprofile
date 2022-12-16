a5a61e6683d6465d2161cf06ba21cc9d13d28fcf
usnikb <usnikbiswas.work@gmail.com>
2022-12-15 19:59:08 -0500

    Changes done to gitprofile.conf

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'usnikb', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
@@ -11,53 +11,47 @@ const config = {
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
+    linkedin: 'usnik-biswas',
+    twitter: 'usnikb',
     facebook: '',
     instagram: '',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: 'usnikb',
+    dev: 'usnikb',
     stackoverflow: '', // format: userid/username
-    website: 'https://arifszn.github.io',
+    website: 'https://usnikb.github.io',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'usnikbiswas.work@gmail.com',
   },
   resume: {
     fileUrl:
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
+    'AWS',
+    'Docker',
+    'Git',
     'PHP',
     'Laravel',
     'JavaScript',
     'React.js',
     'Node.js',
-    'Nest.js',
     'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
+      company: 'Octalian',
+      position: 'Intern',
+      from: 'December 2018',
+      to: 'April 2019',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Accenture',
+      position: 'Software Engineer',
+      from: 'June 2019',
+      to: 'June 2021',
+      companyLink: 'https://accentuer.com',
     },
   ],
   /* certifications: [
@@ -70,22 +64,23 @@ const config = {
   ], */
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Lambton College, Toronto',
+      degree: 'Post-Grad Certificate',
+      from: '2022',
+      to: '2023',
+      currentGPA: '3.5'
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Karunya University, Coimbatore',
+      degree: 'Batcherlors Degree',
+      from: '2015',
+      to: '2019',
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    username: 'usnikb', // to hide blog section, keep it empty
     limit: 3, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
